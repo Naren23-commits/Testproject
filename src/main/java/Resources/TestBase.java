ac4cf409527278994f4ed3bb47471a408248fcfc
Naren23-commits <61700578+Naren23-commits@users.noreply.github.com>
2020-03-04 19:35:43 +0530

    Update TestBase.java

--- src/main/java/Resources/TestBase.java
+++ src/main/java/Resources/TestBase.java
@@ -30,7 +30,7 @@ public WebDriver initializeDriver() throws IOException
 //
 //if(browserName.equals("chrome"))
 //{
-	System.setProperty("webdriver.chrome.driver","\\usr\\local\\bin\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver","chromedriver");
 	return driver= new ChromeDriver();
 	
 }
